0a1e7ddb00d1de6e22a7b207bc47213a8bfc1ce6
mzrpour <125277638+babayiu@users.noreply.github.com>
2023-11-07 05:43:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,6 @@ foreach ($colors as $color) {
 $botidd = "";
 $token = API_KEY;
 $channel = "@Source_Home";//ایدی کانال
-$channel2 = "@Source_Home";//ایدی کانال دوم
 $forchannel = json_decode(file_get_contents("https://api.telegram.org/bot$token/getChatMember?chat_id=@$channel&user_id=".$chat_id));
 $tch = $forchannel->result->status;
 $forchannel2 = json_decode(file_get_contents("https://api.telegram.org/bot$token/getChatMember?chat_id=@$channel2&user_id=".$chat_id));
